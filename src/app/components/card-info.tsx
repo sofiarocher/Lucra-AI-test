ea7a3318af54ea51b817867a5598e502cfe7e162
Sofía Rocher <76635239+sofiarocher@users.noreply.github.com>
2024-04-06 01:13:50 -0300

    fixed space bug

--- src/app/components/card-info.tsx
+++ src/app/components/card-info.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 export default function Card({ title, image, alt, classname }: CardProps) {
     return (
-        <div className={`text-white flex flex-col items-center justify-center bg-gray-300/5 border-1 border-blue-300/30 rounded-xl pt-4 px-4 lg:w-auto w-full${classname}`}>
+        <div className={`text-white flex flex-col items-center justify-center bg-gray-300/5 border-1 border-blue-300/30 rounded-xl pt-4 px-4 lg:w-auto w-full ${classname}`}>
             <p className="font-semibold lg:text-xl text-sm">{title} </p>
             <Image src={image} alt={alt} className="saturate-150 contrast-100 brightness-[90%] w-[130px] lg:w-[200px] "/>
         </div>
