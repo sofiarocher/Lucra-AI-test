198470dfb2adcb8af4ecd6ae97f42eb9d09c1ca9
Sofía Rocher <76635239+sofiarocher@users.noreply.github.com>
2024-04-06 13:51:19 -0300

    styled info modal

--- src/app/components/help-modal.tsx
+++ src/app/components/help-modal.tsx
@@ -1,11 +1,13 @@
 import { MouseEvent } from 'react';
+import Image from 'next/image';
 import ModalContent from './modal-info';
+import Close from "../../../public/lucra-cross.svg"
+import LucraH2 from "../../../public/lucra-help-2.png"
 interface ModalHelpProps {
   onClose: () => void;
 }
 
 export default function ModalHelp({ onClose }: ModalHelpProps) {
-    
     const handleOutsideClick = (event: MouseEvent<HTMLDivElement>) => {
         if (event.currentTarget === event.target) {
           onClose();
@@ -14,7 +16,8 @@ export default function ModalHelp({ onClose }: ModalHelpProps) {
 
     return (
       <div className="fixed inset-0 bg-black bg-opacity-20 flex items-center justify-center p-4" onClick={handleOutsideClick}>
-        <div className="rounded-lg p-8 text-white/90 bg-gray-300/5 border-1 border-blue-300/30 text-center w-1/3" onClick={(e) => e.stopPropagation()}>
+        <div className="rounded-lg p-8 text-white/90 bg-gray-300/5 border-1 border-blue-300/30 text-center w-1/3 relative" onClick={(e) => e.stopPropagation()}>
+            <Image src={Close} alt="Close Icon"  onClick={onClose} className='absolute right-4 top-4 opacity-70'/>
             <h2 className='text-4xl font-bold text-transparent bg-clip-text bg-text-gradient'>How to use Lucra?</h2>
             <ModalContent 
               title="Reasons to use Lucra" 
@@ -27,11 +30,12 @@ export default function ModalHelp({ onClose }: ModalHelpProps) {
               content="Lorem ipsum dolor sit, amet consectetur adipisicipipsum dolor sit, amet consectetur adipisicing elit..sum dolor sit, amet consectetur adipisicing elit..ing elit..." />
             <ModalContent 
               title="Text Generation" 
-              content="Lorem ipsum dolor sit, amet consectetur adipisicingipsum dolor sit, amet consectetur adipisicing elit.. elit..." />
+              content="Lorem ipsum dolor sit, amet consectetur adipisicingipsum dolor sit, amet consectetur adipisicing elit..ipsum dolor sit, amet consectetur adipisicing elit.. elit..." />
             <ModalContent 
               title="Image Generation" 
-              content="Lorem ipsum dolor sit, amet consectetur adipisicing ipsum dolor sit, amet consectetur adipisicing elit..elit..." />
-          <button onClick={onClose}>Cerrar</button>
+              content="Lorem ipsum dolor sit, amet consectetur adipisicing ipsum dolor sit, amet consectetur adipisicing"
+              classname='pr-12' />
+            <Image src={LucraH2} alt="Lucra AI Help Model" className='saturate-150 contrast-100 brightness-[90%] w-[80px] lg:w-[100px] absolute right-0 bottom-0'/>
         </div>
       </div>
     );
--- src/app/components/modal-info.tsx
+++ src/app/components/modal-info.tsx
@@ -1,12 +1,13 @@
 
 interface InfoProps {
     title: string;
-    content: string ;
+    content: string;
+    classname?: string;
 }
 
-export default function ModalContent({ title, content }: InfoProps) {
+export default function ModalContent({ title, content, classname }: InfoProps) {
     return (
-      <div className='text-start m-auto my-4'>
+      <div className={`text-start m-auto my-4 ${classname}`}>
         <p className='font-semibold pt-4'>{title}</p>
         <p>{content}</p>
       </div>
