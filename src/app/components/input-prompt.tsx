11723165214473e98171a49952c3762470fe2605
Sofía Rocher <76635239+sofiarocher@users.noreply.github.com>
2024-04-06 19:18:49 -0300

    added gemini API

--- src/app/components/input-prompt.tsx
+++ src/app/components/input-prompt.tsx
@@ -12,7 +12,6 @@ type PromptProps = {
 };
 
 export default function Prompt({ handleFocus, classname }: PromptProps)  {
-
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [promptValue, setPromptValue] = useState('');
 
@@ -32,6 +31,59 @@ export default function Prompt({ handleFocus, classname }: PromptProps)  {
             handleEnterClick();
         }
     };
+
+    const {
+      GoogleGenerativeAI,
+      HarmCategory,
+      HarmBlockThreshold,
+    } = require("@google/generative-ai");
+    
+    const MODEL_NAME = "gemini-1.0-pro";
+    const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+    
+    async function runChat() {
+      const genAI = new GoogleGenerativeAI(API_KEY);
+      const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+    
+      const generationConfig = {
+        temperature: 0.9,
+        topK: 1,
+        topP: 1,
+        maxOutputTokens: 2048,
+      };
+    
+      const safetySettings = [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+      ];
+    
+      const chat = model.startChat({
+        generationConfig,
+        safetySettings,
+        history: [
+        ],
+      });
+    
+      const result = await chat.sendMessage(promptValue);
+      const response = result.response;
+      console.log(response.text());
+    }
+    
+    runChat();
 
     return (
       <div className={`flex w-full mb-6 md:mb-0 gap-4 ${classname}`}>
